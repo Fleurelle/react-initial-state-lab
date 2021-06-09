4bc7f5bcbdf248524bb1d747b9ea4adf6a1e27ce
Rose <r.d.jeanlouis01@gmail.com>
2021-06-09 00:24:45 -0400

    Completed assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+    //1. The initial state of Bomb should have a property called secondsLeft.
+    //2.The initial value of secondsLeft should be equal to the initialCount 
+    //prop of our Bomb component.
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        if (this.state.secondsLeft === 0) {
+           return (
+               <p>Boom!</p>
+           )
+        } else {
+            return (
+                <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+            )
+        }
+
+    }
+
+}
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,34 @@
 // your ImageSlider code here!
+import React, {Component} from 'react';
+
+class ImageSlider extends Component {
+    //1. Its initial state should have 
+    //a property called currentSlideIndex that starts at 0.
+    constructor() {
+        super();
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    //2. Bomb countdown!!
+    decrement = () => {
+        console.log(`before setState: ${this.state.currentSlideIndex}`)
+
+        this.setState({
+            currentSlideIndex: this.state.currentSlideIndex - 1
+        })
+
+        console.log(`after setState: ${this.state.currentSlideIndex}`)
+    }
+
+    render() {
+        return (
+            <p>I am on slide {this.state.currentSlideIndex}</p>
+        )
+
+    }
+}
+
+export default ImageSlider;
+
